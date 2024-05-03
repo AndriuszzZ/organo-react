6e992b5582d31fdbca40c0a8dd83b52f091a3bb1
AndriuszzZ <andre.genolima@gmail.com>
2024-05-03 17:00:25 -0300

    lista

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
 
       <Banner />
       <Form />
+      
     </div>
   )
 }
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,28 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Form.css'
 
 const Form = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX/UI',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+
     return (
         <section className="form">
             <form>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite seu gargo"/>
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <CampoTexto label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto label="Cargo" placeholder="Digite seu gargo" />
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label="Time" itens={times} />
             </form>
         </section>
     )
